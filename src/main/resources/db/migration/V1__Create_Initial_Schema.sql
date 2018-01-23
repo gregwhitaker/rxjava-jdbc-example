933a94ddc662213beeb9bfbcf75abf8065b09f5a
gregwhitaker <gwhitake@gmail.com>
2018-01-23 13:18:53 -0800

    Doing work

---
+++ src/main/resources/db/migration/V1__Create_Initial_Schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE department
+(
+  department_id   BIGINT NOT NULL PRIMARY KEY,
+  department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE employee (
+  employee_id         BIGINT          NOT NULL PRIMARY KEY,
+  employee_firstname  VARCHAR (100)   NOT NULL,
+  employee_lastname   VARCHAR (100)   NOT NULL,
+  department_id       BIGINT          NOT NULL
+);
+
+INSERT INTO department (department_id, department_name) VALUES (1, 'Sales');
+INSERT INTO department (department_id, department_name) VALUES (2, 'Marketing');
+INSERT INTO department (department_id, department_name) VALUES (3, 'Human Resources');
+INSERT INTO department (department_id, department_name) VALUES (4, 'Manufacturing');
+INSERT INTO department (department_id, department_name) VALUES (5, 'Accounting');
+
+INSERT INTO employee (employee_id, employee_firstname, employee_lastname, department_id) VALUES (1, 'Bob', 'Smith', 4);
+INSERT INTO employee (employee_id, employee_firstname, employee_lastname, department_id) VALUES (2, 'John', 'Green', 1);
+INSERT INTO employee (employee_id, employee_firstname, employee_lastname, department_id) VALUES (3, 'Sally', 'Wilson', 3);
+INSERT INTO employee (employee_id, employee_firstname, employee_lastname, department_id) VALUES (4, 'Harold', 'Smith', 2);
+INSERT INTO employee (employee_id, employee_firstname, employee_lastname, department_id) VALUES (5, 'Joe', 'White', 5);
+INSERT INTO employee (employee_id, employee_firstname, employee_lastname, department_id) VALUES (6, 'Arnold', 'Shoemaker', 4);
