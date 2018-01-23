a3f803cf22e34ff548ea907683f5047a0afe5ce4
gregwhitaker <gwhitake@gmail.com>
2018-01-23 13:04:37 -0800

    Doing work

--- src/main/java/com/github/gregwhitaker/rxjavajdbc/example/Main.java
+++ src/main/java/com/github/gregwhitaker/rxjavajdbc/example/Main.java
@@ -1,6 +1,13 @@
 package com.github.gregwhitaker.rxjavajdbc.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Starts the rxjava-jdbc-example application.
+ */
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String... args) throws Exception {
 
